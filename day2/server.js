fdad2a4754f8fbbfa165ed9731be8f512a4796f3
praveen <praveenmech483@gmail.com>
2023-12-02 10:10:05 +0530

    first commit

---
+++ day2/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+let notes = [
+    {
+        id: 1,
+        content: 'backend using node.js',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'node.js is a open source',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'simple web server using node.js',
+        important: true
+    }
+];
+
+const app = http.createServer((request, response) => {
+    response.stateCode = 200;
+    response.setHeader('content-type', 'appliaction/json');
+    response.end(JSON.stringify(notes));
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
