4e0d3cdc7f8ce01d8177a1444ee4f69cfe619143
praveen <praveenmech483@gmail.com>
2023-12-06 11:09:40 +0530

    day 3 modified code

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const notesRouter = require('./controllers/notes');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/notes', notesRouter);
+
+module.exports = app;
+
+
+
